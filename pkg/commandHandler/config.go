7feab09e15295f642c98216b76b82a9734915f5d
Wiibleyde <nathan@bonnell.fr>
2025-04-30 09:35:49 +0200

    feat: enhance ConfigHandler with improved error handling and response logic for get and delete commands
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- pkg/commandHandler/config.go
+++ pkg/commandHandler/config.go
@@ -2,6 +2,7 @@ package commandHandler
 
 import (
 	"errors"
+	"main/pkg/bot_utils"
 	"main/pkg/data"
 	"main/pkg/logger"
 	"main/prisma/db"
@@ -10,7 +11,6 @@ import (
 )
 
 func ConfigHandler(s *discordgo.Session, i *discordgo.InteractionCreate) error {
-	// Check if we have subcommands
 	if len(i.ApplicationCommandData().Options) == 0 {
 		return errors.New("no subcommand provided")
 	}
@@ -18,12 +18,10 @@ func ConfigHandler(s *discordgo.Session, i *discordgo.InteractionCreate) error {
 	subCommand := i.ApplicationCommandData().Options[0]
 	switch subCommand.Name {
 	case "set":
-		// Check if we have the required options for the set command
 		if len(subCommand.Options) < 2 {
 			return errors.New("missing required options for set command")
 		}
 
-		// Safely access options
 		var strConfig string
 
 		if subCommand.Options[0].Type == discordgo.ApplicationCommandOptionString {
@@ -67,10 +65,7 @@ func ConfigHandler(s *discordgo.Session, i *discordgo.InteractionCreate) error {
 			}
 		}
 
-		embed := &discordgo.MessageEmbed{
-			Description: "Configuration mise à jour avec succès.",
-			Color:       0x00FF00,
-		}
+		embed := bot_utils.SuccessEmbed(s, "Configuration mise à jour avec succès.")
 		err = s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 			Type: discordgo.InteractionResponseChannelMessageWithSource,
 			Data: &discordgo.InteractionResponseData{
@@ -78,6 +73,84 @@ func ConfigHandler(s *discordgo.Session, i *discordgo.InteractionCreate) error {
 			},
 		})
 		return err
+	case "get":
+		client, ctx := data.GetDBClient()
+		configs, err := client.Config.FindMany(
+			db.Config.GuildID.Equals(i.GuildID),
+		).Exec(ctx)
+		if err != nil {
+			logger.ErrorLogger.Println("Error fetching config:", err)
+			return err
+		}
+		if len(configs) == 0 {
+			embed := bot_utils.WarningEmbed(s, "Aucune configuration trouvée pour ce serveur.")
+			err = s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Embeds: []*discordgo.MessageEmbed{embed},
+					Flags:  discordgo.MessageFlagsEphemeral,
+				},
+			})
+			if err != nil {
+				logger.ErrorLogger.Println("Error responding to interaction:", err)
+				return err
+			}
+			return nil
+		}
+		embed := bot_utils.BasicEmbedBuilder(s)
+		embed.Title = "Configurations du serveur"
+		embed.Description = "Voici les configurations actuelles du serveur :"
+		for _, config := range configs {
+			embed.Fields = append(embed.Fields, &discordgo.MessageEmbedField{
+				Name:   config.Key,
+				Value:  config.Value,
+				Inline: false,
+			})
+		}
+		err = s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Embeds: []*discordgo.MessageEmbed{embed},
+				Flags:  discordgo.MessageFlagsEphemeral,
+			},
+		})
+		if err != nil {
+			logger.ErrorLogger.Println("Error responding to interaction:", err)
+			return err
+		}
+		return nil
+	case "delete":
+		if len(subCommand.Options) < 1 {
+			return errors.New("missing required options for delete command")
+		}
+		// Safely access options
+		var strConfig string
+		if subCommand.Options[0].Type == discordgo.ApplicationCommandOptionString {
+			strConfig = subCommand.Options[0].StringValue()
+		} else {
+			return errors.New("config key must be a string")
+		}
+		client, ctx := data.GetDBClient()
+		_, err := client.Config.FindMany(
+			db.Config.GuildID.Equals(i.GuildID),
+			db.Config.Key.Equals(strConfig),
+		).Delete().Exec(ctx)
+		if err != nil {
+			logger.ErrorLogger.Println("Error deleting config:", err)
+			return err
+		}
+		embed := bot_utils.SuccessEmbed(s, "Configuration supprimée avec succès.")
+		err = s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Embeds: []*discordgo.MessageEmbed{embed},
+				Flags:  discordgo.MessageFlagsEphemeral,
+			},
+		})
+		if err != nil {
+			logger.ErrorLogger.Println("Error responding to interaction:", err)
+			return err
+		}
 	}
 	return nil
 }
