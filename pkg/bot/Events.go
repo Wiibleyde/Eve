f6bba4ad0a6597b0c67296c19c567740ef67e39b
Wiibleyde <nathan@bonnell.fr>
2025-04-30 15:02:00 +0200

    fix: remove debug logging for command type in interaction handling
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- pkg/bot/Events.go
+++ pkg/bot/Events.go
@@ -42,7 +42,6 @@ func interactionCreate(s *discordgo.Session, i *discordgo.InteractionCreate) {
 			}
 			// Log based on the command type
 			cmdType := i.ApplicationCommandData().CommandType
-			logger.DebugLogger.Println("Command type:", cmdType)
 			if cmdType == discordgo.UserApplicationCommand {
 				logger.InfoLogger.Println("User command executed:", i.ApplicationCommandData().Name, "by", i.Member.User.Username+" on "+i.ApplicationCommandData().TargetID)
 			} else if cmdType == discordgo.MessageApplicationCommand {
