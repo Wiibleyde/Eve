7ba14f31e9e2de71cb631c956b1f64df0d49c2d2
Wiibleyde <nathan@bonnell.fr>
2025-05-18 20:47:24 +0200

    feat: Enhance ping and talk commands with improved response handling and embed generation
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- bot/commands/utils/ping.ts
+++ bot/commands/utils/ping.ts
@@ -1,11 +1,61 @@
-import { CommandInteraction, SlashCommandBuilder } from "discord.js";
+import { CommandInteraction, MessageFlags, SlashCommandBuilder } from "discord.js";
 import type { ICommand } from "../command";
+import { basicEmbedGenerator } from "../../utils/embeds";
+
+const PING_THRESHOLD = {
+    VERY_GOOD: 50,
+    GOOD: 100,
+    CORRECT: 150,
+    WEAK: 200,
+    BAD: 500,
+};
 
 export const ping: ICommand = {
     data: new SlashCommandBuilder()
         .setName("ping")
         .setDescription("Replies with Pong!"),
     execute: async (interaction: CommandInteraction) => {
-        await interaction.reply("Pong!");
+        const firstResponse = await interaction.deferReply({ withResponse: true, flags: [MessageFlags.Ephemeral] });
+        let status: string;
+        let color: number;
+
+        const memoryData: NodeJS.MemoryUsage = process.memoryUsage();
+        const ping: number = interaction.client.ws.ping;
+
+        if (!firstResponse || ping <= 0) {
+            status = 'Surprenant ! ⚫';
+            color = 0xffffff;
+        } else if (ping < PING_THRESHOLD.VERY_GOOD) {
+            status = 'Très bon 🟢';
+            color = 0x00ff00;
+        } else if (ping < PING_THRESHOLD.GOOD) {
+            status = 'Bon 🟢';
+            color = 0x00ff00;
+        } else if (ping < PING_THRESHOLD.CORRECT) {
+            status = 'Correct 🟡';
+            color = 0x00ff00;
+        } else if (ping < PING_THRESHOLD.WEAK) {
+            status = 'Faible 🟠';
+            color = 0xffa500;
+        } else if (ping < PING_THRESHOLD.BAD) {
+            status = 'Mauvais 🔴';
+            color = 0xff0000;
+        } else {
+            status = 'Très mauvais 🔴';
+            color = 0xff0000;
+        }
+
+        const pingEmbed = basicEmbedGenerator()
+        pingEmbed.setTitle('Ping')
+        pingEmbed.setDescription('Le bot est connecté et répond')
+        pingEmbed.addFields(
+            { name: 'Ping', value: `${ping}ms / ${status}`, inline: true },
+            { name: 'Mémoire', value: `${(memoryData.heapUsed / 1024 / 1024).toFixed(2)} MB`, inline: true },
+            { name: 'Uptime', value: `${(process.uptime() / 60).toFixed(2)} minutes`, inline: true }
+        )
+        pingEmbed.setColor(color)
+
+        await interaction.editReply({ embeds: [pingEmbed] });
+
     }
 };
--- bot/commands/utils/talk.ts
+++ bot/commands/utils/talk.ts
@@ -1,5 +1,7 @@
-import { CommandInteraction, SlashCommandBuilder } from "discord.js";
+import { CommandInteraction, MessageFlags, SlashCommandBuilder, TextChannel, User } from "discord.js";
 import type { ICommand } from "../command";
+import { errorEmbedGenerator, successEmbedGenerator } from "../../utils/embeds";
+import { logger } from "../../..";
 
 export const talk: ICommand = {
     data: new SlashCommandBuilder()
@@ -9,13 +11,39 @@ export const talk: ICommand = {
             option.setName("message")
                 .setDescription("The message to send")
                 .setRequired(true)
+        )
+        .addUserOption(option =>
+            option.setName("mp")
+                .setDescription("The user to send the message to")
         ),
     execute: async (interaction: CommandInteraction) => {
-        const message = interaction.options.get("message")?.value as string;
-        if (message) {
-            await interaction.reply(message);
+        // Defer reply immediately to acknowledge the interaction
+        await interaction.deferReply({ flags: [MessageFlags.Ephemeral] });
+
+        const message = interaction.options.get('message')?.value as string;
+        const member = interaction.options.get('mp')?.user as User | null;
+
+        if (member) {
+            try {
+                await member.send(message);
+            } catch (error) {
+                logger.error(error);
+                await interaction.editReply({
+                    embeds: [errorEmbedGenerator("Impossible d'envoyer le message")],
+                });
+                return;
+            }
         } else {
-            await interaction.reply("Please provide a message.");
+            const channel = interaction.channel as TextChannel;
+            if (!channel) {
+                await interaction.editReply({
+                    embeds: [errorEmbedGenerator('Impossible de trouver le salon de discussion')],
+                });
+                return;
+            }
+            await channel.send(message);
         }
+        await interaction.editReply({ embeds: [successEmbedGenerator('Message envoyé')] });
+
     }
 };
