21175e9f105a46757fe2e09eac306b9ad0c496cb
Wiibleyde <nathan@bonnell.fr>
2025-11-20 13:02:42 +0100

    fix: fucking things

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Run database migrations
-npx prisma migrate deploy
+node_modules/.bin/prisma migrate deploy
 
 # Start the application with exec to properly handle signals
 exec node index.js
